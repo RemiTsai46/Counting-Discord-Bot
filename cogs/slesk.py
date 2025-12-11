f2d313c6e76bf356b4b6bd84bac04f43a7a3394a
RemiTsai46 <remitsai46@gmail.com>
2025-12-11 19:02:57 +0800

    add slash cmd test

---
+++ cogs/slesk.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class GeneralCmds(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="hello", description="graetar")
+    async def hello(self, interaction: discord.Interaction):
+        await interaction.response.send_message("alle vaerld")
+ 
+async def setup(bot):
+    await bot.add_cog(GeneralCmds(bot))
--- main.py
+++ main.py
@@ -9,61 +9,29 @@ token = os.getenv('TOKEN')
 intents = discord.Intents.all()
 intents.message_content = True
 
-#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix="k!",intents=intents)
+
+async def load_extensions():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.command
+async def sync(ctx):
+    bot.tree.copy_global_to(guild = ctx.guild)
+    synced = await bot.tree.sync(guild = ctx.guild)
+    await ctx.send("synced")
 
 @bot.event
 async def on_ready():
     print("Bot is up!")
 
-
-@bot.event
-async def on_message(message):
-
-    if message.author == bot.user:
-        return
-  
-    '''if message.content == "hello":
-        await message.reply("hello") '''
-
-    msgDelta = False # ?
-    num = [0, 0] # curr, prev
-    name = ["", ""] # curr, prev
-    messages = [[message.content,message.author] async for message in message.channel.history(limit=1000)]
-
-    for msg in messages:
-        if msg[1].bot == False:
-            try:
-                numDelta = int(msg[0])
-                nameDelta = msg[1]
-            except ValueError:
-                continue
-            else:
-                if msgDelta == True:
-                    num[1] = numDelta
-                    name[1] = nameDelta
-                    if num[0] != num[1] + 1:
-                        await message.channel.send(f"Hey! You counted wrong! The next number is: {num[1] + 1}", delete_after = 5, reference = message)
-                        await message.delete()
-                        msgDelta = False
-                        messages = None
-                    elif name[0] == name[1]:
-                        await message.channel.send(f"Hey! You can't count twice! The next number is: {num[1] + 1}", delete_after = 5, reference = message)
-                        await message.delete()
-                    num = [0, 0]
-                    name = ["", ""]
-                    break
-                else:
-                    num[0] = numDelta
-                    name[0] = nameDelta
-                    msgDelta = True
-        else:
-            continue 
-    
-    await bot.process_commands(message)
-
-@bot.command()
-async def test(ctx, arg1, arg2):
-    await ctx.send(arg1 + arg2)
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(token)
+ 
+if __name__ == '__main__':
+    asyncio.run(main())
 
 bot.run(token)
